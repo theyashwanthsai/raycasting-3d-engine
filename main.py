1fbc313d3c46646c39bf89600dc4e1ccef409600
Sai Yashwanth <saiyashwanth@Sais-MacBook-Air.local>
2022-12-27 17:56:14 +0530

    v1

---
+++ main.py
@@ -0,0 +1,48 @@
+import pygame 
+import sys
+from settings import *
+from map import *
+from player import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(RES)
+        self.clock = pygame.time.Clock()
+        self.delta_time = 1
+        self.new_game()
+        
+    def new_game(self):
+        self.map = Map(self)
+        self.player = Player(self)
+    
+    def update(self):
+        self.player.update()
+        pygame.display.flip()
+        self.delta_time = self.clock.tick(FPS)
+        pygame.display.set_caption(str(self.clock.get_fps()))
+        
+        
+    def draw(self):
+        self.screen.fill('black')
+        self.map.draw()
+        self.player.draw()
+        
+        
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            
+            
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+            
+            
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ player.py
@@ -0,0 +1,76 @@
+from settings import *
+import pygame
+import math
+
+
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+        
+    def movement(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
+        speed = PLAYER_SPEED*self.game.delta_time
+        speed_sin = speed*sin_a
+        speed_cos = speed*cos_a
+        
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            dx += speed_cos
+            dy += speed_sin
+            
+        if keys[pygame.K_s]:
+            dx += -speed_cos
+            dy += -speed_sin
+            
+        if keys[pygame.K_a]:
+            dx += speed_cos
+            dy += -speed_sin 
+            
+        if keys[pygame.K_d]:
+            dx += -speed_cos
+            dy += speed_sin
+            
+        # self.x += dx
+        # self.y += dy
+        
+        self.check_wall_collision(dx, dy)
+        
+        
+        if keys[pygame.K_LEFT]:
+            self.angle -= PLAYER_ROT_SPEED*self.game.delta_time
+            
+        if keys[pygame.K_RIGHT]:
+            self.angle += PLAYER_ROT_SPEED*self.game.delta_time
+            
+        self.angle %= math.tau
+        
+        
+    def check_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+    
+    def check_wall_collision(self, dx, dy):
+            if self.check_wall(int(self.x + dx), int(self.y)):
+                self.x += dx
+            if self.check_wall(int(self.x), int(self.y + dy)):
+                self.y += dy
+        
+    def draw(self):
+        pygame.draw.line(self.game.screen, 'yellow', (self.x*100, self.y*100), (self.x*100 + WIDTH*math.cos(self.angle), self.y*100 + WIDTH*math.sin(self.angle)), 2) 
+        
+        pygame.draw.circle(self.game.screen, 'green', (self.x*100, self.y*100), 15)
+    
+    def update(self):
+        self.movement()
+        
+    @property
+    def pos(self):
+        return self.x, self.y
+    
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
