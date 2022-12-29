b80633045f2f48a00eec5f48f62bf052d0c88949
Yashwanth Sai <68785131+theyashwanthsai@users.noreply.github.com>
2022-12-29 15:53:37 +0530

    3d depth

--- raycasting.py
+++ raycasting.py
@@ -62,9 +62,23 @@ class RayCasting:
                               
                               
                               
-            # Draw rays for debugging
-            pygame.draw.line(self.game.screen, 'yellow', (100*ox, 100*oy), (100*ox + 100*depth*cos_a, 100*oy + 100*depth*sin_a), 2)              
+            # # Draw rays for debugging
+            # pygame.draw.line(self.game.screen, 'yellow', (100*ox, 100*oy), (100*ox + 100*depth*cos_a, 100*oy + 100*depth*sin_a), 2)              
+                 
+            # Remove the fishbowl effect
+            depth *= math.cos(self.game.player.angle - ray_angle)     
+                 
+                 
                             
+            # 3D projection
+            proj_height = SCREEN_DIST / (depth + 0.0001)
+            
+            
+            
+            ## Draw 3d walls     
+            
+            color = [255/(1+depth**5*0.00002)]*3                     
+            pygame.draw.rect(self.game.screen, color, (ray*SCALE, HALF_HEIGHT-proj_height//2, SCALE, proj_height))
                             
 
             ray_angle += DELTA_ANGLE
